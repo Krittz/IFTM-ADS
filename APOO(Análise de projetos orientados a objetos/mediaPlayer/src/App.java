bfea5fcb3aed0f1c5cd3db5119f91e96153eb91d
Cristian <55900416+Krittz@users.noreply.github.com>
2023-05-12 12:23:30 -0300

    Delete APOO(Análise de projetos orientados a objetos/mediaPlayer directory

--- APOO(Análise de projetos orientados a objetos/mediaPlayer/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-
-  public static void main(String[] args) throws Exception {
-    AudioPlayer player = new AudioPlayer();
-    player.play("mp4", "movie.mp4");
-    player.play("mp3", "song.mp3");
-  }
-}
--- APOO(Análise de projetos orientados a objetos/mediaPlayer/src/Mp4Player.java
+++
@@ -1,7 +0,0 @@
-
-public class Mp4Player {
-
-  public void playMp4(String fileName) {
-    System.out.println("Playing mp4 file. Name: " + fileName);
-  }
-}
