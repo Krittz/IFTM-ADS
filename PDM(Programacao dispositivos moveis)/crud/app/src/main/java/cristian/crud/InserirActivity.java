d7224746944f56ced676c81808149658d9aedd10
cristian <cristianalvessilvak@gmail.com>
2023-11-10 14:45:55 -0300

    Finalizando layouts

--- PDM(Programacao dispositivos moveis)/crud/app/src/main/java/cristian/crud/InserirActivity.java
+++ PDM(Programacao dispositivos moveis)/crud/app/src/main/java/cristian/crud/InserirActivity.java
@@ -3,27 +3,37 @@ package cristian.crud;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class InserirActivity extends AppCompatActivity {
+    AppCompatButton btnGravar;
+
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir);
-    }
-    public void adicionar(View view){
-        EditText edtNome = findViewById();
-        EditText edtTelefone = findViewById();
-        String nome = edtNome.getText().toString().trim();
-        String telefone = edtTelefone.getText().toString().trim();
+        btnGravar = findViewById(R.id.btnGravar);
+        btnGravar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText edtNome = findViewById(R.id.inputNome);
+                EditText edtTelefone = findViewById(R.id.inputPhone);
+                String nome = edtNome.getText().toString().trim();
+                String telefone = edtTelefone.getText().toString().trim();
 
-        Contato c = new Contato (0, nome, telefone);
-        DBHelper dbHelper = new DBHelper(getApplicationContext());
-        c.setId(dbHelper.inserirContato(c));
-        if(c.getId() >0){
-            Toast.makeText(getApplicationContext(), "Inserido com sucesso", Toast.LENGTH_LONG).show();
-        }
+                Contato c = new Contato(0, nome, telefone);
+                DBHelper dbHelper = new DBHelper(getApplicationContext());
+                c.setId(dbHelper.inserirContato(c));
+                if (c.getId() > 0) {
+                    Toast.makeText(getApplicationContext(), "Inserido com sucesso", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+
 }
--- PDM(Programacao dispositivos moveis)/crud/app/src/main/java/cristian/crud/MainActivity.java
+++ PDM(Programacao dispositivos moveis)/crud/app/src/main/java/cristian/crud/MainActivity.java
@@ -1,20 +1,29 @@
 package cristian.crud;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    AppCompatButton redirectBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        redirectBtn = (AppCompatButton) findViewById(R.id.redirectCreate);
+
+        redirectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, InserirActivity.class);
+                startActivity(i);            }
+        });
     }
-    public  void inserir(View v){
-        Intent i = new Intent(MainActivity.this, InserirActivity.class);
-        startActivity(i);
-    }
+
+
 }
