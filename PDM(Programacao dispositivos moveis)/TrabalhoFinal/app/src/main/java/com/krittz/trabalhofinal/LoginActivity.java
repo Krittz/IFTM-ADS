053789593dfc4587b55378034d9ece990c2611e6
Cristian <cristianalvessilvak@gmail.com>
2023-11-18 21:23:05 -0300

    Add btn back

--- PDM(Programacao dispositivos moveis)/TrabalhoFinal/app/src/main/java/com/krittz/trabalhofinal/LoginActivity.java
+++ PDM(Programacao dispositivos moveis)/TrabalhoFinal/app/src/main/java/com/krittz/trabalhofinal/LoginActivity.java
@@ -6,15 +6,25 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
 
 public class LoginActivity extends AppCompatActivity {
 AppCompatButton btnLogin, btnCadastrar;
+AppCompatImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         btnLogin = findViewById(R.id.btnLogin);
         btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
--- PDM(Programacao dispositivos moveis)/TrabalhoFinal/app/src/main/java/com/krittz/trabalhofinal/SignUpActivity.java
+++ PDM(Programacao dispositivos moveis)/TrabalhoFinal/app/src/main/java/com/krittz/trabalhofinal/SignUpActivity.java
@@ -6,15 +6,24 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
 
 public class SignUpActivity extends AppCompatActivity {
 AppCompatButton btnLogin;
+AppCompatImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
         btnLogin = findViewById(R.id.btnLogin);
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
