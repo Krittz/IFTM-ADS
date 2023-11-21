38d22142c3a44f38343c7d60f193d4f75bce420b
Cristian <cristianalvessilvak@gmail.com>
2023-11-20 22:04:15 -0300

    Remove charts fails

--- PDM(Programacao dispositivos moveis)/TrabalhoFinal/app/build.gradle.kts
+++ PDM(Programacao dispositivos moveis)/TrabalhoFinal/app/build.gradle.kts
@@ -28,7 +28,8 @@ android {
 }
 
 dependencies {
-    implementation("com.github.lecho:hellocharts-library:1.5.8@aar")
+
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("org.mindrot:jbcrypt:0.4")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -37,3 +38,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
