98ea6d6de66abbe7f79e17479c7e65bafe20c123
Krittz <cristianalvessilvak@gmail.com>
2024-02-21 21:56:03 -0300

    Add novo roteiro pdm2

---
+++ PDM(Programacao dispositivos moveis)/app/src/main/java/br/com/cran/MainActivity.java
@@ -0,0 +1,43 @@
+package br.com.cran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requisicao();
+    }
+
+    public void requisicao() {
+        final TextView textView = (TextView) findViewById(R.id.text);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://www.cran.com.br";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textView.setText("Resposta: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("Algo deu errado!");
+            }
+        });
+        queue.add(stringRequest);
+    }
+}
