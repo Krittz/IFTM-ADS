b9f108bdbd09f8ecfd0e34e940053b24e444ba95
Krittz <cristianalvessilvak@gmail.com>
2024-02-23 21:24:55 -0300

    Novo roteiro de JSON request

---
+++ PDM(Programacao dispositivos moveis)/JsonRequest/app/src/main/java/br/com/cran/MainActivity.java
@@ -0,0 +1,45 @@
+package br.com.cran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requisicao();
+    }
+
+    public void requisicao() {
+        final TextView textView = (TextView) findViewById(R.id.tv);
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        textView.setText("Resposta: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("Algo deu errado!");
+                    }
+                }
+        );
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+}
---
+++ PDM(Programacao dispositivos moveis)/JsonRequest/app/src/main/java/br/com/cran/MySingleton.java
@@ -0,0 +1,36 @@
+package br.com.cran;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MySingleton {
+    private static MySingleton instancia;
+    private RequestQueue requestQueue;
+    private static Context ctx;
+
+    private MySingleton(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized MySingleton getInstance(Context context) {
+        if (instancia == null) {
+            instancia = new MySingleton(context);
+        }
+        return instancia;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
