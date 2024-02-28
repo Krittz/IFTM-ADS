c1a59ef8fba180c72912e02aafda4c5058013b8d
Krittz <cristianalvessilvak@gmail.com>
2024-02-28 19:58:04 -0300

    Adicionando nova materia

---
+++ DPA(Desenvolvimento de Projetos Aplicados)/BaoOuNao/src/main/java/br/com/cran/BaoOuNao/BaoOuNaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cran.BaoOuNao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoOuNaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoOuNaoApplication.class, args);
+	}
+
+}
