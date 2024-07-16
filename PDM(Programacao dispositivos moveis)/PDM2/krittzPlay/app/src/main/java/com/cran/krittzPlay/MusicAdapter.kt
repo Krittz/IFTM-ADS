57f66c7af9ff048895a7dca97564b1d784b885e9
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-07-16 03:13:42 -0300

    Add File Description on MusicApp

--- PDM(Programacao dispositivos moveis)/PDM2/krittzPlay/app/src/main/java/com/cran/krittzPlay/MusicAdapter.kt
+++ PDM(Programacao dispositivos moveis)/PDM2/krittzPlay/app/src/main/java/com/cran/krittzPlay/MusicAdapter.kt
@@ -3,7 +3,7 @@ package com.cran.krittzPlay
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -11,9 +11,42 @@ class MusicAdapter(
     private val musicList: List<Music>,
     private val onItemClick: (Music) -> Unit
 ) : RecyclerView.Adapter<MusicAdapter.MusicViewHolder>() {
-    class MusicViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    inner class MusicViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val titleMusic: AppCompatTextView = view.findViewById(R.id.musicName)
         val artistName: AppCompatTextView = view.findViewById(R.id.artistName)
+        val filePath: AppCompatTextView = view.findViewById(R.id.filePath)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onItemClick(musicList[position])
+                }
+            }
+
+            itemView.setOnLongClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    showMusicDetailsDialog(musicList[position])
+                }
+                true // Indica que o evento foi consumido
+            }
+        }
+
+        private fun showMusicDetailsDialog(music: Music) {
+            val context = itemView.context
+            val dialogBuilder = AlertDialog.Builder(context)
+            dialogBuilder.setTitle("Detalhes da Música")
+            dialogBuilder.setMessage("Nome: ${music.title}\nArtista: ${music.artist}\nCaminho: ${music.filePath}")
+
+            dialogBuilder.setPositiveButton("OK") { dialog, which ->
+                dialog.dismiss()
+            }
+
+            val dialog = dialogBuilder.create()
+            dialog.show()
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicViewHolder {
@@ -25,9 +58,7 @@ class MusicAdapter(
         val music = musicList[position]
         holder.titleMusic.text = music.title
         holder.artistName.text = music.artist
-        holder.itemView.setOnClickListener {
-            onItemClick(music)
-        }
+        holder.filePath.text = music.filePath
     }
 
     override fun getItemCount() = musicList.size
