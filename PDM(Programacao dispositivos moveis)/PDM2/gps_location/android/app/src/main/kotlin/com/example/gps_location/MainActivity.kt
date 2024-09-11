c998514db7ea3d4bb3d5ffe7be79a9a5e3f60a55
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-09-11 11:39:53 -0300

    java widget ok

---
+++ PDM(Programacao dispositivos moveis)/PDM2/gps_location/android/app/src/main/kotlin/com/example/gps_location/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gps_location
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ PDM(Programacao dispositivos moveis)/PDM2/location/android/app/src/main/kotlin/br/com/cran/location/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.cran.location
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
